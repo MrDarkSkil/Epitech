63eae6088e610c4629b2291899ee3184e01595fb
Hubert Léo <leohub@live.fr>
2017-04-10 20:14:51 +0200

    Remove LibLapin

--- LibLapin/include/lapin_enum.h
+++
@@ -1,9 +0,0 @@
-/*
-** Exists only because its existed from 1.0 to 1.6
-*/
-
-#ifndef			__LAPIN_ENUM_COMPAT_H__
-# define		__LAPIN_ENUM_COMPAT_H__
-# include		"lapin.h"
-#endif	/*		__LAPIN_ENUM_COMPAT_H__	*/
-
